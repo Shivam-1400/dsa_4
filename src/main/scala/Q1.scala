f342ac79a1f7753fcf7d2976daa3404b2180e123
Shivam Keshri <skshivam.keshri852@gmail.com>
2024-04-22 15:03:31 +0530

    first commit

---
+++ src/main/scala/Q1.scala
@@ -0,0 +1,24 @@
+import scala.annotation.tailrec
+class BestTimeToBuyAndSellTheStock extends App {
+    def maxProfit(prices: Array[Int]): Int = {
+      if (prices.isEmpty) {
+        0
+      } else {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      }
+    }
+}
